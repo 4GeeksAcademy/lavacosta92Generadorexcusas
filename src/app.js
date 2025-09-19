8d58706aeaad0e611f1488f73e53eda4504013ec
Lavacosta92 <lorena07acosta@gmail.com>
2025-09-19 19:26:46 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const subjects = ["My dog aura -.-", "My son liam -.-", "My cousin >.<", "My little sister -.-"];
+const verbs    = ["smash", "broke", "hid", "delete"];
+const objects  = ["The homework", "the computer", "the presentation", "the car"];
+const places   = ["in the librery", "at home", "at school", "on Mars"];
+
+function getRandom(arr) {
+  const idx= Math.floor(Math.random()* arr.length)
+  return arr[idx];
+}
+
+function generateExcuse() {
+  const s = getRandom(subjects);
+  const v = getRandom(verbs);
+  const o = getRandom(objects);
+  const p = getRandom(places);
+
+  const excuse = `${s} ${v} ${o} ${p}.`;
+  const parrafo =document.getElementById("excusa");
+  parrafo.innerHTML = excuse;
+}
+  generateExcuse();
+}
+
+
